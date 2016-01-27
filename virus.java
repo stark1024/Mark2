1ff9ba4606ce9652fc9385df80d6b6cf9ca82ec2
Rishabh Jaiswal <rishabhj1024@gmail.com>
2016-01-27 12:59:21 +0530

    Update virus.java
    Virus :P
    will work more efficiently now!

--- virus.java
+++ virus.java
@@ -57,11 +57,11 @@ public class virus extends javax.swing.JFrame {
             getContentPane().setLayout(layout);
             layout.setHorizontalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGap(0, 400, Short.MAX_VALUE)
+                .addGap(0, 2000, Short.MAX_VALUE)
             );
             layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGap(0, 300, Short.MAX_VALUE)
+                .addGap(0, 1500, Short.MAX_VALUE)
             );
 
             pack();
